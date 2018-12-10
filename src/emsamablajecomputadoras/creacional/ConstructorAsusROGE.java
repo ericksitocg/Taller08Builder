a0ec7508e2328f3bf9ee25e058b543e78deecdf5
Tonny1494 <33798401+Tonny1494@users.noreply.github.com>
2018-12-10 12:01:35 -0500

    se implementaron los constructores especificos

---
+++ src/emsamablajecomputadoras/creacional/ConstructorAsusROGE.java
@@ -0,0 +1,45 @@
+package emsamablajecomputadoras.creacional;
+
+public class ConstructorAsusROGE extends ConstructorComputadora {
+
+	@Override
+	public void darMarca() {
+		// TODO Auto-generated method stub
+		computador.setMarca("Asus");
+		
+	}
+
+	@Override
+	public void darModelo() {
+		// TODO Auto-generated method stub
+		computador.setModelo("ROG");
+	}
+
+	@Override
+	public void darRam() {
+		// TODO Auto-generated method stub
+		computador.setRam(32);
+	}
+
+	@Override
+	public void darAlmacenamiento() {
+		// TODO Auto-generated method stub
+		computador.setAlmacenamiento(1000);
+		
+	}
+
+	@Override
+	public void darSistemaOperativo() {
+		// TODO Auto-generated method stub
+		computador.setOs(new SistemaOperativo("Windows",10,"home"));
+		
+	}
+
+	@Override
+	public void darMainboard() {
+		// TODO Auto-generated method stub
+		computador.setPlaca(new Mainboard("Strix ","x99"));
+		
+	}
+	
+}
