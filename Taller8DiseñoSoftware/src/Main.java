a1a521fe78b744e011fcfaa6bb04b971b7dd9042
Erick Cordova Gavilanes <erihucor@espol.edu.ec>
2018-12-10 19:00:38 -0500

    main

--- Taller8DiseñoSoftware/src/Main.java
+++ Taller8DiseñoSoftware/src/Main.java
@@ -5,25 +5,30 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Apéndice de método generado automáticamente
 		ConstructorComputadoraDirector director = new ConstructorComputadoraDirector();
-		ConstructorComputadora constructor;
+		ConstructorComputadora constructor= null;
 		
-		int respuesta;
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Ingrese la opcion:\n1)AsusROGE\n2)AsusZenbook\n");
-		respuesta = sc.nextInt();
-
-		if(respuesta==1) {
-			constructor = new ConstructorAsusROGE();
+		int respuesta = -1;
+		while (respuesta!=3) {
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Ingrese la opcion:\n1)AsusROGE\n2)AsusZenbook\n3)Salir");
+			respuesta = sc.nextInt();
+	
+			if(respuesta==1) {
+				constructor = new ConstructorAsusROGE();
+			}
+			else if(respuesta == 2) {
+				constructor = new ConstructorAsusZenbook();
+			}
+			if(respuesta!=3) {
+				
+				director.establecerConstructor(constructor);
+				director.construirComputadora();
+				
+				Computador computador = director.obtenerComputadora();
+				System.out.println("*********************");
+				System.out.println(computador.especificacionesTecnicas());
+				System.out.println("*********************");
+			}
 		}
-		else {
-			constructor = new ConstructorAsusZenbook();
-		}
-		
-		director.establecerConstructor(constructor);
-		director.construirComputadora();
-		
-		Computador computador = director.obtenerComputadora();
-		System.out.println(computador.especificacionesTecnicas());
 	}
-
 }
